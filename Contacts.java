069c2b1fce419f95a9425016d275bbf9ff959bbb
Dereck Rascon <dereckrascon76@gmail.com>
2023-01-17 11:29:36 -0700

    input code to show contacts inside data folder

--- Contacts.java
+++ Contacts.java
@@ -1,2 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+
+
 public class Contacts {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        String directory = "data";
+        String filename = "contacts.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+        if(Files.notExists(dataDirectory)){
+            Files.createDirectories(dataDirectory);
+        }
+
+        if(! Files.exists(dataFile)){
+            Files.createFile(dataFile);
+        }
+    }
+
+
+
+
+
+
+
+
 }
+
